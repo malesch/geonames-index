dc08555014aa119f2899ce1f08294d5422ed3d73
Marcus Spiegel <malex@calclab.com>
2016-07-04 01:26:03 +0200

    Location spec definition

---
+++ src/geonames_index/location.clj
@@ -0,0 +1,21 @@
+(ns geonames-index.location
+  (:require [clojure.spec :as s]))
+
+(s/def ::name string?)
+(s/def ::alternate-names (s/map-of keyword? ::name))
+(s/def ::country (s/and string? #(= (count %) 2)))
+(s/def ::population integer?)
+
+(s/def ::lon float?)
+(s/def ::lat float?)
+(s/def ::coordinates (s/keys :req-un [::lon ::lat]))
+
+(s/def ::class (s/and string? #(= (count %) 1)))
+(s/def ::code (s/and string? #(< (count %) 11)))
+(s/def ::admin1 string?)
+(s/def ::admin2 (s/nilable string?))
+(s/def ::admin3 (s/nilable string?))
+(s/def ::admin4 (s/nilable string?))
+(s/def ::classification (s/keys :req-un [::class ::code ::admin1 ::admin2 ::admin3 ::admin4]))
+
+(s/def ::location (s/keys :req-un [::name ::alternate-names ::coordinates ::country ::population ::classification]))
