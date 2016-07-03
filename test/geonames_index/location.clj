f7b7d930817f829c480e439e4d4d56c123483110
Marcus Spiegel <malex@calclab.com>
2016-07-03 16:54:56 +0200

    Add location data example (as expected from DB queries)

---
+++ test/geonames_index/location.clj
@@ -0,0 +1,27 @@
+(ns geonames-index.location)
+
+;; Example data structure extracted from the database
+
+(def example-location {:name "Milano"
+                       :alternate-names {:de "Mailand"
+                                         :es "Milán"
+                                         :ar "ميلانو"
+                                         :bg "Милано"
+                                         :fr "Milan"
+                                         :hu "Milánó"
+                                         :zh-TW "米蘭"
+                                         :ja "ミラノ"
+                                         :ta "மிலன்"
+                                         :ug "مىلان"
+                                         :scn "Milanu"}
+                       :coordinates {:lon 9.1895100
+                                     :lat 45.4642700}
+                       :country "IT"
+                       :population 1236837
+                       :classification {:class "P"
+                                        :code "PPLA"
+                                        :admin1 "09"
+                                        :admin2 "MI"
+                                        :admin3 "015146"
+                                        :admin4 nil}})
+
